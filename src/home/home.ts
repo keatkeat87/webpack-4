b1a9cd48a50435721bf1cefec9978bb242d11d57
Mark Koo <koo_cool88@hotmail.com>
2018-10-16 18:21:27 +0800

    ok

--- src/home/home.ts
+++ src/home/home.ts
@@ -1,17 +1,31 @@
 
 import '../shared/a.scss';
 import '../shared/b.scss';
+import '@fancyapps/fancybox/dist/jquery.fancybox.min.css';
+import 'owl.carousel/dist/assets/owl.carousel.min.css'
 import { a } from '../shared/a';
 import { b } from '../shared/b';
-console.log(a + 'da');
-console.log(b + 'ta');
+// console.log(a + 'da');
+// console.log(b + 'ta');
 
-//  import * as $ from 'jquery';
-//  console.log($);
-//  import '@fancyapps/fancybox';
+import * as $ from 'jquery';
+import '@fancyapps/fancybox';
+import 'owl.carousel/dist/owl.carousel';
+import 'isotope-layout';
 
-// console.log($.fancybox);
+$('#owl-carousel').owlCarousel({
+    items: 1,
+    loop: true,
+    center: true,
+    dots: true,
+    dotsData: true,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    animateOut: 'fadeOut'
+});
 
-// $("#home").html(`
-//     <div>home page</div>
-// `);
+$('#masonry').isotope({
+    itemSelector: '.masonry-item',
+    layoutMode: 'masonry'
+});
--- webpack.common.js
+++ webpack.common.js
@@ -1,26 +1,27 @@
- const path = require('path');
- const HtmlWebpackPlugin = require('html-webpack-plugin'); // create index.html
- const CleanWebpackPlugin = require('clean-webpack-plugin');
- const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // split out css 
- //  const InlineSourcePlugin = require('html-webpack-inline-source-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // create index.html
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // split out css 
+const webpack = require('webpack');
+//  const InlineSourcePlugin = require('html-webpack-inline-source-plugin');
 
- module.exports = {
-     entry: {
-         home: './src/home/home.ts',
-         about: './src/about/about.ts',
-         contact: './src/contact/contact.ts',
-         //  service: './src/service/service.ts',
-         //  app: './src/index.ts',
-         polyfills: './src/polyfills.ts'
-     },
-     plugins: [
-         new CleanWebpackPlugin(['dist']),
-         new HtmlWebpackPlugin({
-             template: './src/home/template.html',
-             inject: false,
-             filename: path.resolve(__dirname, 'src/home/index.html'),
-             sName: 'home'
-         }),
+module.exports = {
+    entry: {
+        home: './src/home/home.ts',
+        about: './src/about/about.ts',
+        contact: './src/contact/contact.ts',
+        //  service: './src/service/service.ts',
+        //  app: './src/index.ts',
+        polyfills: './src/polyfills.ts'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            template: './src/home/template.html',
+            inject: false,
+            filename: path.resolve(__dirname, 'src/home/index.html'),
+            sName: 'home'
+        }),
          new HtmlWebpackPlugin({
              template: './src/about/template.html',
              inject: false,
@@ -33,90 +34,107 @@
              filename: path.resolve(__dirname, 'src/contact/index.html'),
              sName: 'contact'
          }),
-         // new InlineSourcePlugin(),
-         // new HtmlWebpackPlugin({
-         //     template: './src/service/template.html',
-         //     inject: false,
-         //     chunks: ['service'],
-         //     filename: path.resolve(__dirname, 'src/service/index.html')
-         // }),
-         new MiniCssExtractPlugin({
-             filename: "[name][contenthash].css",
-             chunkFilename: "[name][contenthash].css"
-         })
-     ],
-     output: {
-         publicPath: "/",
-         filename: '[name].[chunkhash].bundle.js',
-         path: path.resolve(__dirname, 'dist')
-     },
-     // vendor
-     optimization: {
-         runtimeChunk: true,
-         splitChunks: {
-             chunks(chunk) {
-                 return chunk.name !== 'polyfills' && chunk.name !== 'landingPage';
-             },
-             minSize: 1,
-             minChunks: 1,
-             maxAsyncRequests: 5,
-             maxInitialRequests: 2,
-             automaticNameDelimiter: '~',
-             name: true,
-             cacheGroups: {
-                 vendors: {
-                     test: /[\\/]node_modules[\\/]/,
-                     priority: -10
-                 },
-                 default: {
-                     minChunks: 2,
-                     priority: -20,
-                     reuseExistingChunk: true
-                 }
-             }
-         }
-     },
-     resolve: {
-         extensions: ['.ts', '.js']
-     },
-     module: {
-         rules: [{
-                 test: /\.ts?$/,
-                 use: 'ts-loader',
-                 exclude: /node_modules/
-             },
-             {
-                 test: /\.scss$/,
-                 use: [{
-                     loader: process.env.NODE_ENV === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
-                 }, {
-                     loader: 'css-loader',
-                     options: {
-                         sourceMap: true
-                     }
-                 }, {
-                     loader: 'sass-loader',
-                     options: {
-                         sourceMap: true
-                     }
-                 }]
-             },
-             {
-                 test: /\.(png|svg|jpg|gif)$/,
-                 use: [{
-                     loader: 'url-loader', // base64
-                     options: {
-                         limit: 8192,
-                         fallback: 'file-loader'
-                     }
-                 }]
-             },
-             {
-                 test: /\.(woff|woff2|eot|ttf|otf)$/,
-                 use: [
-                     'file-loader'
-                 ]
-             }
-         ]
-     }
- };
+        // new InlineSourcePlugin(),
+        // new HtmlWebpackPlugin({
+        //     template: './src/service/template.html',
+        //     inject: false,
+        //     chunks: ['service'],
+        //     filename: path.resolve(__dirname, 'src/service/index.html')
+        // }),
+        new MiniCssExtractPlugin({
+            filename: '[name][contenthash].css',
+            chunkFilename: '[name][contenthash].css'
+        }),
+        new webpack.ProvidePlugin({
+           jQuery: 'jquery',
+           'window.jQuery': 'jquery'
+        })
+    ],
+    output: {
+        publicPath: '/dist',
+        filename: '[name].[chunkhash].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    // vendor
+    optimization: {
+        runtimeChunk: true,
+        splitChunks: {
+            chunks(chunk) {
+                return chunk.name !== 'polyfills' && chunk.name !== 'landingPage';
+            },
+            minSize: 1,
+            minChunks: 1,
+            maxAsyncRequests: 5,
+            maxInitialRequests: 2,
+            automaticNameDelimiter: '~',
+            name: true,
+            cacheGroups: {
+                vendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: -10
+                },
+                default: {
+                    minChunks: 2,
+                    priority: -20,
+                    reuseExistingChunk: true
+                }
+            }
+        }
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [{
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [{
+                    loader: process.env.NODE_ENV === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
+                }, {
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                }]
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: process.env.NODE_ENV === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
+                }, {
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                }, {
+                    loader: 'sass-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                }]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [{
+                    loader: 'url-loader', // base64
+                    options: {
+                        limit: 8192,
+                        fallback: 'file-loader',
+                        outputPath: '/',
+                        publicPath: '/dist/'
+                    }
+                }]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    }
+};
