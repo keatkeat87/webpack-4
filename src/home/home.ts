b52d7d46cd7828c40a63276254051edb7e898b43
Mark Koo <koo_cool88@hotmail.com>
2018-10-16 22:33:26 +0800

    ok

--- src/home/home.ts
+++ src/home/home.ts
@@ -4,6 +4,7 @@ import '../shared/b.scss';
 import '@fancyapps/fancybox/dist/jquery.fancybox.min.css';
 import 'owl.carousel/dist/assets/owl.carousel.min.css';
 import 'datatables.net-dt/css/jquery.dataTables.min.css';
+import 'datatables.net-responsive-dt/css/responsive.dataTables.min.css';
 import { a } from '../shared/a';
 import { b } from '../shared/b';
 // console.log(a + 'da');
@@ -12,13 +13,12 @@ import { b } from '../shared/b';
 import * as $ from 'jquery';
 import '@fancyapps/fancybox';
 import 'owl.carousel/dist/owl.carousel';
-// import * as Isotope from 'isotope-layout'; //module, type 
-import 'isotope-layout'; 
-declare let Isotope : any;
-import 'datatables.net.dt';
-import 'datatables.net-responsive-dt';
+import * as Isotope from 'isotope-layout';
+import  'datatables.net';
+import 'datatables.net-responsive';
 import 'datatables.net-scroller';
 import 'datatables.net-select';
+import 'jquery-form';
 
 $('[data-fancybox]').fancybox();
 
@@ -33,12 +33,46 @@ $('#owl-carousel').owlCarousel({
     autoplayHoverPause: true,
     animateOut: 'fadeOut'
 });
- 
 
 new Isotope('#masonry', {
     itemSelector: '.masonry-item',
     layoutMode: 'masonry'
 });
 
-// $('#table').DataTable();
+let table =  $('#table').DataTable({
+    scrollY: '100px',
+    responsive: true,
+    scroller: true,
+    select: true
+});
+setTimeout(()=>{
+    table.row( 10 ).scrollTo();
+},1000);
+
+$('.form').ajaxForm({
+    success: function (response, textStatus, xhr, form) {
+        console.log("in ajaxForm success");
+        jQuery('.myFormLoading').hide();
+        jQuery('.myFormError').hide();
+        jQuery('.myFormSuccess').show();
+        jQuery('input,textarea').val('');
+
+        console.log("open gtag")
+        //gtag_contactFormSubmitted();
+    },
+    error: function (xhr, textStatus, errorThrown) {
+        jQuery('.myFormLoading').hide();
+        jQuery('.myFormError').show();
+        console.log("in ajaxForm error");
+    },
+    complete: function (xhr, textStatus) {
+        console.log("in ajaxForm complete");
+    },
+    beforeSend: function () {
+        jQuery('.myFormDefault').hide();
+        jQuery('.myFormLoading').show();
+        console.log("before send");
+    }
+});
+
 
