eaa53fee4385783895d1e02495e59ade14d788c1
Derrick Yam <keatkeat87@live.com>
2018-10-17 23:11:27 +0800

    ok

--- src/modules/googleAnalytics.ts
+++ src/modules/googleAnalytics.ts
@@ -13,32 +13,34 @@ export function setupGA(googleAnalyticsId: string) {
     document.getElementsByTagName('head')[0].appendChild(script);
 
  
-    // let script2 = document.createElement('script') as HTMLScriptElement;
-    // script2.type = 'text/javascript';
-    // script2.textContent = `
-    //     window.dataLayer = window.dataLayer || [];
-    //     function gtag(){dataLayer.push(arguments);}
-    //     gtag('js', new Date());    
-    //     gtag('config', '${ googleAnalyticsId }');
-    // `;
-    // document.getElementsByTagName('head')[0].appendChild(script2);
+    let script2 = document.createElement('script') as HTMLScriptElement;
+    script2.type = 'text/javascript';
+    script2.textContent = `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());    
+        gtag('config', '${ googleAnalyticsId }');
+    `;
+    document.getElementsByTagName('head')[0].appendChild(script2);
 
     
-    window.dataLayer = window.dataLayer || [];
-    function gtag(...args: any[]){window.dataLayer.push(arguments);}
-    gtag('js', new Date());    
-    gtag('config', '${ googleAnalyticsId }', {
-        'custom_map': {
-            'dimension1': 'userIsAdmin',
-            'dimension2': 'scrolled'
-        }
-    });
+    // window.dataLayer = window.dataLayer || [];
+    // function gtag(...args: any[]){window.dataLayer.push(arguments);}
+    // gtag('js', new Date());    
+    // gtag('config', '${ googleAnalyticsId }', {
+    //     'custom_map': {
+    //         'dimension1': 'userIsAdmin',
+    //         'dimension2': 'scrolled'
+    //     }
+    // });
 
     setTimeout(() => {
+        
         gtag('event', 'aaa', {
             'event_category' : 'bbb',
             'event_label' : 'ccc'
         });
+        alert('done');
     },5000);
 
     // document.querySelectorAll<HTMLElement>('.googleAnalyticsTracking').forEach(elem => {
